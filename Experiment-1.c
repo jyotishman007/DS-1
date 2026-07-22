e766caa5f5e473d8d7814ffa36a494d308f61413
jyotishman007 <dasjyotishman85@gmail.com>
2026-07-22 12:00:06 +0530

    Update Experiment-1.c

--- Experiment-1.c
+++ Experiment-1.c
@@ -43,3 +43,31 @@ void main()
 
     
 }
+// SEcttion 2
+#include <stdio.h>
+
+#define Max 10
+
+int main()
+{
+    int n;
+    int Stack[Max];
+    int top = -1;
+    int i;
+
+    printf("Enter the Number of Elements: ");
+    scanf("%d", &n);
+
+    if (top == (Max - 1))
+    {
+        printf("Stack is full\n");
+    }
+    else
+    {
+        top++;
+        Stack[top] = n;  
+        printf("Element %d pushed successfully.\n", Stack[top]);
+    }
+
+    return 0;
+}
